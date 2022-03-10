2c9bb949eb47be59433a8aa41440ba6b2b6ce26e
Everton Rocha <irocha10@hotmail.com>
2022-03-10 10:55:15 -0300

    Side menu backdrop

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,16 @@ const navbar = document.querySelector(".navbar");
 
 btn.addEventListener("click", (e) => {
   navbar.classList.toggle("sidebar-open");
+
+  if (navbar.classList.contains("sidebar-open")) {
+    const backdrop = document.createElement("div");
+    backdrop.classList.add("backdrop");
+    backdrop.addEventListener("click", (e) => {
+      navbar.classList.remove("sidebar-open");
+      document.querySelector(".backdrop").remove();
+    });
+    document.querySelector("body").append(backdrop);
+  } else {
+    document.querySelector(".backdrop").remove();
+  }
 });
